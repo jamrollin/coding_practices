0dc81eeebdee00b8ba9041f2b56eaed150173065
bebor <200354806+jamrollin@users.noreply.github.com>
2025-08-06 01:05:26 +0800

    Add multiply.js

---
+++ practice-solutions/javascript/multiply.js
@@ -0,0 +1,17 @@
+/**
+ * Write a function called multiply that takes 2 numbers and returns their product.
+ * 
+ * @param {Number} multiply - Input numbers
+ * @param {Number} - Multiply numbers
+ */
+
+            
+let a = Number (prompt("Give me a number:", " "));
+alert(a);
+let b = Number(prompt("Give me a number:", " "));
+
+function multiply (a, b){
+return a * b;
+}
+
+(multiply (a, b));
