f2ac5b1613e64692a8d1428c1428f7c27db2514c
bebor <200354806+jamrollin@users.noreply.github.com>
2025-08-06 00:58:06 +0800

    Add Add_7_to_n

---
+++ practice-solutions/javascript/Add_7_to_n.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function called add7 that takes one number and returns that number + 7.
+ * 
+ * @param {Number} add7 - input number
+ * @returns {Number} - Add numbers
+ */
+
+function add7 (n) {
+        return n + 7;
+    }
+    
+    console.log((add7(5)));
